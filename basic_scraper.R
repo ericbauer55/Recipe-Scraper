502fc0335872c679ebb7dacc852a2606826d2314
vulcant <eric.bauer55@gmail.com>
2019-03-28 22:52:43 -0400

    essential structure is put in place. gotta make sure that the recipes list isn't all NAs.

---
+++ basic_scraper.R
@@ -0,0 +1,42 @@
+###################################
+# Setup
+###################################
+setwd("~/R_Programming/R_code/Recipe-Scraper")
+src_dir <- file.path(getwd(),"data")
+library(dplyr)
+library(readr)
+library(rvest)
+
+
+###################################
+# Read LUT for CSS selectors
+###################################
+filenames <- list.files(src_dir)
+filenames
+css_sel <- file.path(src_dir,"recipe_site_css_selectors.csv")
+css_sel <- read_csv(css_sel)
+
+###################################
+# Scrape HTML
+###################################
+linkfile1 <- file.path(src_dir,"links_lunch_dishes.txt")
+lf1 <- read_lines(linkfile1) # read the links into a character vector
+
+recipes <- list()
+count <- 1
+for(link in lf1){
+    # check which website from the CSS LUT is being scraped
+    css_site <- css_sel[sapply(css_sel$website, function(y) grepl(y,link)), ]
+    # read the html
+    h <- read_html(link)
+    
+    # scrape the html into a list
+    tmp <- list()
+    for(node in names(css_site)){
+        tmp[[node]] <- h %>% html_node(node) %>% html_text() # scrape each node
+    }
+    recipes[[count]] <- tmp
+    count <- count + 1
+}
+
+str(recipes)
