0163aad3b54591beae800218de7f8891fa419f6a
vulcant <eric.bauer55@gmail.com>
2019-03-29 18:10:25 -0400

    made the scraping functional based on a single link.

--- basic_scraper.R
+++ basic_scraper.R
@@ -21,22 +21,36 @@ css_sel <- read_csv(css_sel)
 ###################################
 linkfile1 <- file.path(src_dir,"links_lunch_dishes.txt")
 lf1 <- read_lines(linkfile1) # read the links into a character vector
-
+lnk <- lf1[1] # select the first option
+css_site <- css_sel[sapply(css_sel$website, function(y) grepl(y,lnk)), ]
 recipes <- list()
+# scrape the html into a list
+tmp <- list()
 count <- 1
-for(link in lf1){
-    # check which website from the CSS LUT is being scraped
-    css_site <- css_sel[sapply(css_sel$website, function(y) grepl(y,link)), ]
-    # read the html
-    h <- read_html(link)
-    
-    # scrape the html into a list
-    tmp <- list()
-    for(node in names(css_site)){
-        tmp[[node]] <- h %>% html_node(node) %>% html_text() # scrape each node
+for(node in names(css_site)[-1]){ # [-1] removes website name
+    if(node == "ingredients"){
+        tmp[[node]] <- h %>% html_nodes(css_site[[node]]) %>% html_text() # scrape each node 
+    } else {
+        tmp[[node]] <- h %>% html_node(css_site[[node]]) %>% html_text() # scrape first node 
     }
-    recipes[[count]] <- tmp
-    count <- count + 1
 }
+recipes[[count]] <- tmp
 
-str(recipes)
+# recipes <- list()
+# count <- 1
+# for(link in lf1){
+#     # check which website from the CSS LUT is being scraped
+#     css_site <- css_sel[sapply(css_sel$website, function(y) grepl(y,link)), ]
+#     # read the html
+#     h <- read_html(link)
+#     
+#     # scrape the html into a list
+#     tmp <- list()
+#     for(node in names(css_site)){
+#         tmp[[node]] <- h %>% html_node(node) %>% html_text() # scrape each node
+#     }
+#     recipes[[count]] <- tmp
+#     count <- count + 1
+# }
+# 
+# str(recipes)
